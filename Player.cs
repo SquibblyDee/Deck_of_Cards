d765a5a75df874518f2f0fbabee8316a693b5f07
ste ste <steelst0@gmail.com>
2018-07-29 19:35:59 -0700

    for revert

--- Player.cs
+++ Player.cs
@@ -1,13 +1,33 @@
-// using System;
-// using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+
+namespace Deck_of_Cards
+{
+    class Player
+    {
+        ////Give the Player class a name property.
+        public string name;
+        ////Give the Player a hand property that is a List of type Card.
+        public List<Card> hand = new List<Card>();
+
+        public Player(string playerName="John")
+        {
+            name = playerName;
+        }
+
+        //Give the Player a draw method of which draws a card from a deck, adds it to the player's hand and returns the Card.
+        public Card Draw(ref Deck deck)
+        {
+            Card ourCard = deck.Deal();
+            hand.Add(ourCard);
+            return ourCard;
+        }
+
+        ////Give the Player a discard method which discards the Card at the specified index 
+        ////from the player's hand and returns this Card or null if the index does not exist.
+        // public Card Discard()
+        // {
 // 
-// namespace Deck_of_Cards
-// {
-    // class Player
-    // {
-        //Give the Player class a name property.
-        // public string name;
-        //Give the Player a hand property that is a List of type Card.
-        // public List<Card> hand = new List<Card>();
-    // }
-// }
+        // }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,13 +11,18 @@ namespace Deck_of_Cards
 
             Console.WriteLine("\nDeck of Cards:");
             ////Creating a new deck
-            Deck ourDeck = new Deck();
+            public Deck ourDeck = new Deck();
             ////Dealing to ourselves from the Deck
             Card drawnCard = ourDeck.Deal();
             ////Resetting our deck back to it's default list
             ourDeck.Reset();
             ////Shuffling the deck
             ourDeck.Shuffle();
+            ////Creating a player object
+            Player player1 = new Player("Steve");
+            Console.WriteLine("PLAYEROBJ: "+ player1);
+            ///Have player object draw a card
+            
             
         }
     }
